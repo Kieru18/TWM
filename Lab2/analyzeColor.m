669651f30cfb84c3576a04e3e39601eba96de609
jkierucz <01169183@pw.edu.pl>
2026-03-20 19:49:49 +0100

    moved from rgb heuristic to hsv

--- Lab2/analyzeColor.m
+++ Lab2/analyzeColor.m
@@ -1,18 +1,39 @@
 function colorName = analyzeColor(img, stats_i)
     pixels = stats_i.PixelIdxList;
-    r = mean(img(pixels));
-    g = mean(img(pixels + numel(img)/3));
-    b = mean(img(pixels + 2*numel(img)/3));
-
-    if r > g && r > b && g > (r * 0.5)
-        colorName = 'Yellow';
-    elseif r > g && r > b
-        colorName = 'Red';
-    elseif g > r && g > b
-        colorName = 'Green';
-    elseif b > r && b > g
-        colorName = 'Blue';
+    
+    imgHSV = rgb2hsv(img);
+    
+    hChan = imgHSV(:,:,1);
+    sChan = imgHSV(:,:,2); 
+    vChan = imgHSV(:,:,3);
+    
+    H = mean(hChan(pixels)) * 360;
+    S = mean(sChan(pixels)) * 100;
+    V = mean(vChan(pixels)) * 100;
+    
+    fprintf('ID Stats: H=%.1f, S=%.1f, V=%.1f | ', H, S, V);
+    
+    if (H > 340 || H < 15) && S > 60 && S < 80 && V > 80 && V < 100
+        colorName = "Red";
+    elseif (H > 340 || H < 15) && S <= 60 && S > 40 && V > 80 && V < 100
+        colorName = "Pink";
+    elseif H >= 15 && H < 40 && V < 30
+        colorName = "Black";
+    elseif H >= 15 && H < 40 && S <= 40 && V > 60 && V < 80
+        colorName = "Gray";
+    elseif H >= 15 && H < 40 && S <= 40 && V >= 80
+        colorName = "White";
+    elseif H >= 15 && H < 80 && S > 90
+         colorName = "Yellow";
+    elseif H > 200 && H < 240 && S > 40 && V > 40
+        colorName = "Blue";
+    elseif H > 230 && H < 270 && S < 40 && V < 40
+        colorName = "Dark Blue";
+    elseif H > 60 && H < 110 && S < 20 && V > 60
+        colorName = "Light Blue";
+    elseif H > 100 && H < 160 && S > 20 && S < 60 && V < 70
+        colorName = "Green";
     else
-        colorName = 'Object';
+        colorName = "Other";
     end
 end
