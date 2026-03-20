e85e809a9098482e287ce36700018a0dfa379576
jkierucz <01169183@pw.edu.pl>
2026-03-20 19:49:34 +0100

    updated functions

--- Lab2/analyzeShape.m
+++ Lab2/analyzeShape.m
@@ -1,9 +1,12 @@
 function [shapeName, uiColor] = analyzeShape(stats_i)
-    if stats_i.Circularity > 0.94
+    if stats_i.Circularity >= 0.957
         shapeName = 'Circle';
         uiColor = 'Green';
-    else
+    else if stats_i.Eccentricity < 0.85
         shapeName = 'Square';
         uiColor = 'Red';
+    else
+        shapeName = 'Other';
+        uiColor = 'Black';
     end
 end
--- Lab2/filterMask.m
+++ Lab2/filterMask.m
@@ -1,14 +1,11 @@
 function cleanMask = filterMask(binaryMask)
-    % fill internal holes (black numbers inside the shapes)
-    cleanMask = imfill(binaryMask, 'holes'); 
-    
     % closing operation (dilation -> erosion)
     % connect fragmented parts and smooth internal gaps
-    se_close = strel('disk', 10); 
-    cleanMask = imclose(cleanMask, se_close);
+    se_close = strel('disk', 7); 
+    cleanMask = imclose(binaryMask, se_close);
     
     % opening operation (erosion -> dilation)
     % remove small noise around the objects and smooth rough edges
-    se_open = strel('disk', 5);
+    se_open = strel('disk', 19);
     cleanMask = imopen(cleanMask, se_open);
 end
