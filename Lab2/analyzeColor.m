8b416499289b8a11c240842a1e42fb3af16c166a
Jakub Kieruczenko <97810587+Kieru18@users.noreply.github.com>
2026-03-22 21:28:00 +0100

    color classification

--- Lab2/analyzeColor.m
+++ Lab2/analyzeColor.m
@@ -1,38 +1,51 @@
 function colorName = analyzeColor(img, stats_i)
     pixels = stats_i.PixelIdxList;
-    
     imgHSV = rgb2hsv(img);
     
-    hChan = imgHSV(:,:,1);
-    sChan = imgHSV(:,:,2); 
-    vChan = imgHSV(:,:,3);
+    H_chan = imgHSV(:,:,1);
+    S_chan = imgHSV(:,:,2);
+    V_chan = imgHSV(:,:,3);
     
-    H = mean(hChan(pixels)) * 360;
-    S = mean(sChan(pixels)) * 100;
-    V = mean(vChan(pixels)) * 100;
+    H = mean(H_chan(pixels)) * 360;
+    S = mean(S_chan(pixels)) * 100;
+    V = mean(V_chan(pixels)) * 100;
     
     fprintf('ID Stats: H=%.1f, S=%.1f, V=%.1f | ', H, S, V);
-    
-    if (H > 340 || H < 15) && S > 60 && S < 80 && V > 80 && V < 100
-        colorName = "Red";
-    elseif (H > 340 || H < 15) && S <= 60 && S > 40 && V > 80 && V < 100
-        colorName = "Pink";
-    elseif H >= 15 && H < 40 && V < 30
+
+    if V < 22
         colorName = "Black";
-    elseif H >= 15 && H < 40 && S <= 40 && V > 60 && V < 80
-        colorName = "Gray";
-    elseif H >= 15 && H < 40 && S <= 40 && V >= 80
+    elseif S < 18 && V > 72
         colorName = "White";
-    elseif H >= 15 && H < 80 && S > 90
-         colorName = "Yellow";
-    elseif H > 200 && H < 240 && S > 40 && V > 40
-        colorName = "Blue";
-    elseif H > 230 && H < 270 && S < 40 && V < 40
+
+    elseif (H > 200 && H < 280) && V < 45
         colorName = "Dark Blue";
-    elseif H > 60 && H < 110 && S < 20 && V > 60
-        colorName = "Light Blue";
-    elseif H > 100 && H < 160 && S > 20 && S < 60 && V < 70
+
+    elseif S < 28 && V < 75
+        colorName = "Gray";
+
+    elseif (H >= 120 && H <= 145) && S > 30
         colorName = "Green";
+
+
+    % theroretical HSV ranges are different. 
+    % for some reason in this picture those are correct.
+    elseif (S > 30 && S < 45) && (V > 58) && (H > 80 && H < 265)
+        colorName = "Pink";
+    elseif (H > 300 || H < 20) && S < 55
+        colorName = "Pink";
+        
+    elseif (H > 350 || H < 20) && S >= 55
+        colorName = "Red";
+        
+    elseif H >= 20 && H < 70
+        colorName = "Yellow";
+        
+    elseif H >= 70 && H < 165
+        colorName = "Green";
+        
+    elseif H >= 165 && H < 270
+        colorName = "Blue";
+        
     else
         colorName = "Other";
     end
